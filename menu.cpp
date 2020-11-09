15d155e23f8207e755bdb774a0262571b5e78f23
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-09 17:51:50 +0100

    Filled printMenu()

--- menu.cpp
+++ menu.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 void Menu::printMenu()
 {
-
+    cout<<"Welcome in our amazing library"<<endl;
+    cout<<"l - go to library menu"<<endl;
+    cout<<"a - go to account menu"<<endl;
 }
 
 void Menu::switchMenu()
 {
-    
+
 }
 
 
