14095b29ff89a72aeb173896a6f6ddb2053bcd62
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-12 14:10:51 +0100

    Small correction of string name

--- book.hpp
+++ book.hpp
@@ -5,13 +5,13 @@ using namespace std;
 
 class Book
 {
-    string name; //book name
+    string title; //book name
     string author; //book's author
     bool taken; //state
     
     public:
         Book();  //c-tor
-        Book(string b_name, string b_author, bool b_taken); // overloaded c-tor
+        Book(string b_title, string b_author, bool b_taken); // overloaded c-tor
         void reservation();
         void cancel_reservation();
         void book_status();
