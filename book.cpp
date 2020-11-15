a50bd2239e70daf777e3706b85d615d35731da42
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-15 16:06:05 +0100

    Different idea for class Book

--- book.cpp
+++ book.cpp
@@ -9,63 +9,27 @@ Book::Book()
     taken = false;
 }
 
-Book::Book(string b_title, string b_author, bool b_taken)
+Book::Book(string title, string author)
 {
-    title = b_title;
-    author = b_author;
-    taken = b_taken;
+    title = "" ;
+    author = "";
 }
 
-void Book::reservation()
+string Book::return_title(string title)
 {
-    if(taken)
-    {
-        cout << endl << "This book is already taken. We are so sorry!" << endl;
-    }
-    else 
-    {
-       taken = true;
-       cout << endl << "You have successfully reserved a book!" << endl;
-    }
+    return title;
 }
 
-void Book::cancel_reservation()
+string Book::return_author(string author)
 {
-    if(taken)
-    {
-        taken = false;
-        cout << endl << "You have successfully canceled reservation!" << endl;
-    }
-    else
-    {
-        cout << "This book was not reserved!" << endl;
-    } 
+    return author;
+}
+bool Book::return_taken(bool taken)
+{
+    return taken;
 }
 
-void Book::book_status()
+Book* Book::return_nextBook(Book* nextBook)
 {
-    if(taken) cout << "This book is taken!" << endl;
-    else cout<<"THis book is available for reservation!" << endl;
+    return nextBook;
 }
-
-                 //SMALL TEST OF THE FUNCTIONS
-int main(void)
-{
-    Book book1;
-
-    book1.reservation();
-    cout << "COMMIT: Information about reservation" << endl;
-    cout << "=============================" << endl;
-    book1.book_status();
-    cout << "COMMIT: Should be reserved" << endl;
-    cout << "=============================" << endl;
-    book1.reservation();
-    cout << "COMMIT: Should not be able to reserve" << endl;
-    cout << "=============================" << endl;
-    book1.cancel_reservation();
-    cout << "COMMIT: Informtion about the cancel of reservation" << endl;
-    cout << "=============================" << endl;
-    book1.book_status();
-    cout << "COMMIT: Should be able to be reserved" << endl;
-}
-
