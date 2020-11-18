4b6c183373a8b15127981cbc061f2f092dbe40d2
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-18 20:05:29 +0100

    Beggining of the work with files

---
+++ booktxt.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "booktxt.hpp"
+#include "booklist.cpp"
+
+using namespace std;
+
+void BookTXT::loadFile(BookList &BooksTXT)
+{
+
+};
