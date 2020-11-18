5cc61d27ffbc5c6a3054549936eeeadd9d731fe9
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-18 20:09:49 +0100

    Function to load books from txt file

--- booktxt.cpp
+++ booktxt.cpp
@@ -10,5 +10,19 @@ using namespace std;
 
 void BookTXT::loadFile(BookList &BooksTXT)
 {
+    inFile.open("library.txt");
+    
+    string title, author, line;
 
+    if (inFile.is_open())
+    {
+        while (getline(inFile, line))
+        {
+            istringstream iss(line);
+            if (!(iss >> author >> title)) { break; }
+            BooksTXT.add_book(author, title);
+        }
+    }
+
+    inFile.close();
 };
