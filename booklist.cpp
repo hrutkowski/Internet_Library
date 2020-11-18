5f07e1572d84cb69bfe01e9d0795bc7f7efe0441
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-18 19:48:29 +0100

    Method add_book() for class BookList

--- booklist.cpp
+++ booklist.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include "booklist.hpp"
-#include "book.hpp"
+#include "book.cpp"
 
 using namespace std;
 
@@ -23,4 +23,27 @@ void BookList::search_by_title(string title)
 void BookList::reserve_book(string title, string author)
 {
 
+}
+
+void BookList::add_book(string title, string author)
+{
+    Book *newBook = new Book;
+    newBook->title = title;
+    newBook->author = author;
+    if (firstBook == 0)
+    {
+        firstBook = newBook;
+    }
+    else 
+    {
+        Book *temp = firstBook;
+
+        while (temp->nextBook)
+        {
+            temp = temp->nextBook;
+        }
+
+        temp->nextBook = newBook;
+        newBook->nextBook = 0;
+    }
 }
