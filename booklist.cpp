62f5482cab2eae71478576407489cf29c74754fa
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-21 19:30:34 +0100

    Functionality of books reservation

--- booklist.cpp
+++ booklist.cpp
@@ -17,12 +17,12 @@ void BookList::search_by_author(string author)
     while (temp)
     {
         if (author == temp->get_author())
-            {
-                cout <<  temp->get_author() << " - " << "\"" << temp->get_title() << "\"";
-                if(temp->get_taken()==true) cout << " - TAKEN" << endl;
-                else cout << " - AVAILABLE" << endl;
-                temp = temp->get_nextBook();   
-            }
+        {
+            cout <<  temp->get_author() << " - " << "\"" << temp->get_title() << "\"";
+            if(temp->get_taken()==true) cout << " - TAKEN" << endl;
+            else cout << " - AVAILABLE" << endl;
+            temp = temp->get_nextBook();   
+        }
         else temp = temp->get_nextBook();
     }
 }
@@ -34,19 +34,29 @@ void BookList::search_by_title(string title)
     while (temp)
     {
         if (title == temp->get_title())
-            {
-                cout <<  temp->get_author() << " - " << "\"" << temp->get_title() << "\"";
-                if(temp->get_taken()==true) cout << " - TAKEN" << endl;
-                else cout << " - AVAILABLE" << endl;
-                temp = temp->get_nextBook();   
-            }
+        {
+            cout <<  temp->get_author() << " - " << "\"" << temp->get_title() << "\"";
+            if(temp->get_taken()==true) cout << " - TAKEN" << endl;
+            else cout << " - AVAILABLE" << endl;
+            temp = temp->get_nextBook();   
+        }
         else temp = temp->get_nextBook();
     }
 }
 
-void BookList::reserve_book(string title, string author)
+void BookList::reserve_book(string author, string title)
 {
+    Book *temp = firstBook;
 
+    while (temp)
+    {
+        if (title == temp->get_title())
+        {
+            temp->set_taken(1);
+            temp = temp->get_nextBook();
+        }
+        else temp = temp->get_nextBook();
+    }
 }
 
 void BookList::add_book(string author, string title, bool taken)
