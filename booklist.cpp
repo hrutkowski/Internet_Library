684893de50eae28b9272d435e6b272d251a2821f
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-22 22:50:40 +0100

    Finished messages after functionalities and Messenger instead of cout

--- booklist.cpp
+++ booklist.cpp
@@ -57,40 +57,58 @@ void BookList::reserve_book(string author, string title)
 {
     Book *temp = firstBook;
     Messenger mess;
-    bool success;
+    bool success, taken;
 
     while (temp)
     {
         if (title == temp->get_title() && author == temp->get_author())
         {
-            success=true;
-            temp->set_taken(1);
-            mess.showText("Book reserved successfully!");
-            temp = temp->get_nextBook();
+            if(temp->get_taken()==0)
+            {
+                success=true;
+                temp->set_taken(1);
+                mess.showText("Book reserved successfully!");
+                temp = temp->get_nextBook();
+            }
+            else 
+            {
+                taken=true;
+                break;
+            }
         }
         else temp = temp->get_nextBook();
     }
-    if(success==false) mess.showText("Sorry, something gone wrong. Try again!");
+    if(taken==true) mess.showText("Book is not available. We are so sorry!");
+    else if(success==false) mess.showText("Sorry, something gone wrong. Try again!");
 }
 
 void BookList::return_book(string author, string title)
 {
     Book *temp = firstBook;
     Messenger mess;
-    bool success;
+    bool success, available;
 
     while (temp)
     {
         if (title == temp->get_title() && author == temp->get_author())
         {
+            if(temp->get_taken() == 1)
+            {
             success=true;
             temp->set_taken(0);
             temp = temp->get_nextBook();
             mess.showText("Book returned!");
+            }
+            else 
+            {
+                available=true;
+                break;
+            }
         }
         else temp = temp->get_nextBook();
     }
-    if(success==false) mess.showText("Sorry, something gone wrong. Try again!");
+    if(available==true) mess.showText("Book was not taken!");
+    else if(success==false) mess.showText("Sorry, something gone wrong. Try again!");
 }
 
 void BookList::add_book(string author, string title, bool taken)
