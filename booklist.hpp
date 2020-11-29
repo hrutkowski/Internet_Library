df80d5bf2a42760c27229dc2c7bb3991a85eb546
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2020-11-29 10:43:32 +0100

    Using regular iterator, not a class. Small change in method of writting method names.

--- booklist.hpp
+++ booklist.hpp
@@ -5,27 +5,16 @@
 
 using namespace std;
 
-class BookIt
-{
-    using iterator_type = std::list<Book>::const_iterator;
-    iterator_type iterator_;
-public:
-    BookIt(const iterator_type& it) : iterator_(it) {}
-
-    iterator_type operator->() {return iterator_;}
-    const Book & operator*() {return *iterator_;}
-};
-
 class BookList
 {
     std::list<Book> books_;
 
 public:
     BookList() = default;
-    BookIt search_by_author(string author);
-    BookIt search_by_title(string title);
-    void reserv(BookIt);
-    void ret(BookIt);
+    vector<list<Book>::iterator> searchAuthor(string author);
+    vector<list<Book>::iterator> searchTitle(string title);
+    void reserv();
+    void ret();
     void add(const Book&);
     auto begin() {return books_.begin();}
 };
