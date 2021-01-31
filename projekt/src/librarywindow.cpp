f6eb458c0e946499ed961528853b7c200b5a12fc
Michał Grzegorz Bojke <m.bojke@stud.elka.pw.edu.pl>
2021-01-31 22:00:36 +0100

    Working ListWidget version

--- projekt/src/librarywindow.cpp
+++ projekt/src/librarywindow.cpp
@@ -1,5 +1,6 @@
 #include "librarywindow.hpp"
 #include "ui_librarywindow.h"
+#include <QMessageBox>
 
 
 LibraryWindow::LibraryWindow(Menu &passedMenu, QWidget *parent) :
@@ -14,9 +15,23 @@ LibraryWindow::LibraryWindow(Menu &passedMenu, QWidget *parent) :
 
     for(auto &iter : currentMenu->bookList.listGet())
     {
+     QListWidgetItem *ptr = new QListWidgetItem;
+
      QString temp = QString::fromStdString(iter.title());
-     ui->listWidget->addItem(temp);
+     ptr->setText(temp);
+
+     temp = QString::fromStdString(iter.author());
+     ptr->setData(Qt::ToolTipRole, temp);
+
+     if (iter.isTaken())
+     {
+         temp = "Taken";
+     }
+     else (temp = "Available");
+     ptr->setData(Qt::UserRole, temp);
+     ui->listWidget->addItem(ptr);
     }
+
 
 
 }
@@ -28,7 +43,35 @@ LibraryWindow::~LibraryWindow()
 
 void LibraryWindow::on_pushButtonReserve_clicked()
 {
-    ui->label_2->setText(tr("Book successfully reserved!"));
+    BookIt iter;
+
+    QListWidgetItem* currentItem = ui->listWidget->currentItem();
+
+    QVariant status = currentItem->data(Qt::UserRole);
+    QVariant author = currentItem->data(Qt::ToolTipRole);
+    QVariant title = currentItem->text();
+    QString authorsearch = author.toString();
+    QString titlesearch = title.toString();
+    QString Message = "Author: " + author.toString() + "\nThe book is: " + status.toString();
+
+    QMessageBox messageBox;
+    messageBox.setText(Message);
+    messageBox.exec();
+
+    if (status.toString() == "Available")
+    {
+    currentItem->setData(Qt::UserRole, "Taken");
+    for(auto &iter : currentMenu->bookList.listGet())
+    {
+        if (iter.author() == authorsearch.toStdString() && iter.title() == titlesearch.toStdString())
+        {
+               iter.take();
+               currentMenu->uniloader_book.saveDataBase("Library.txt");
+        }
+    }
+    ui->label_2->setText("Succesfully reserved book!");
+    }
+    else (ui->label_2->setText("Book already reserved!"));
 }
 
 
--- projekt/src/mainwindow.cpp
+++ projekt/src/mainwindow.cpp
@@ -22,9 +22,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::loginButtonClicked()
 {
-    mainMenu.uniloader_account.loadDataBase("AccountDataBase.txt");
     //mainMenu.accountList.displayAccounts();
-    mainMenu.uniloader_book.loadDataBase("library.txt");
     //mainMenu.bookList.displayBooks();
     QString text = ui->textEdit->toPlainText();
     std::string login = text.toStdString();
